e664d6b54f7c104e491cb60e20498bd2f8ffacc0
negarr202020 <36852456+negarr202020@users.noreply.github.com>
2018-03-06 10:03:29 +0330

    Update 3_1_input.py

--- 3_1_input.py
+++ 3_1_input.py
@@ -2,10 +2,10 @@
 # get a string from user #
 ##########################
 text = input("enter what you want\n")
-print(5*text)
+print(15*text)
 
 ##########################
 # get a number from user #
 ##########################
 number=int(input("enter an integer\n"))
-print(5*number)
+print(15557*number)
