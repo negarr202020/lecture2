c86c6eb604177b74d384b49c54cc68b405af2d9b
negarr202020 <36852456+negarr202020@users.noreply.github.com>
2018-03-06 10:02:40 +0330

    Update 2_1_strings.py

--- 2_1_strings.py
+++ 2_1_strings.py
@@ -1,32 +1,32 @@
 ####################################
 # following variables are the same #
 ####################################
-# str1=" salam "
+str1=" slm "
 # str2='salam'
 
 ######################
 ## concat two string #
 ######################
-# name='mohammad'
-# greeting= str1+name
-# print(greeting)
+ name='negarrazavi'
+ greeting= str1+name
+ print(greeting)
 #
 # greeting2=str1+" "+name
 # print(greeting2)
 #
-# print(("\'"+greeting2+" \'") * 3)
+ print(("\'"+greeting1+" \'") * 3)
 ######################################
 ## some useful operations on strings #
 ######################################
-# print(greeting2.capitalize())
-# print(greeting2.upper())
-# print(greeting2.rsplit(' ')) # splitting the string by blank
-# print(greeting2.strip(' ')) # remove the blanks before and after the string
+ print(greeting2.capitalize())
+ print(greeting2.upper())
+ print(greeting2.rsplit(' ')) # splitting the string by blank
+ print(greeting2.strip(' ')) # remove the blanks before and after the string
 
 ###########################################
 ### another sample of printing a varibale #
 ###########################################
-pi = 3.14
+pi = 3
 print(pi)
 str_pi = str(pi)
 print("pi number is : ", pi)  # print each part of input separately
