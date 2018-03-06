72d12375a6bbc6032a4147b59b737cd536677eb0
negarr202020 <36852456+negarr202020@users.noreply.github.com>
2018-03-06 10:07:44 +0330

    Update 7_1_while_loop.py

--- 7_1_while_loop.py
+++ 7_1_while_loop.py
@@ -1,8 +1,8 @@
 ##################################
 ## example of a simple while loop#
 ##################################
-a=input("please enter your name\n")
-while a != "Ali":
+a=input("please enter your name & family\n")
+while a != "negarrazavi":
     print("oops you are not a legal user")
-    a = input("please enter your name\n")
+    a = input("please enter your name & family\n")
 print("thanks for your registration")
